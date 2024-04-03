e079afb79f7734938d76898d4a240c7e33fe3a0d
MrCoderMan1 <arifernan7@gmail.com>
2024-04-02 20:13:24 -0400

    updated form

---
+++ src/script.js
@@ -0,0 +1,20 @@
+let firstName = document.getElementById("firstName");
+let middleName = document.getElementById("middleName");
+let lastName = document.getElementById("lastName");
+let birthday = document.getElementById("birthday");
+
+let gender = document.getElementById("genderSelection");
+let email = document.getElementById("email");
+
+function submit() {
+    
+}
+
+function reset() {
+    firstName.value = "";
+    middleName.value = "";
+    lastName.value = "";
+    birthday.value = "";
+    gender.value = "Other";
+    email.value = "";
+}
